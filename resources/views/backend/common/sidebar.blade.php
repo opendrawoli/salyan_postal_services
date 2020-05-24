78d46a8dc7f3e6fe5560372f0bd37655e6fe3f18
ran bahadur kc <ranbdrkc201@gmail.com>
2020-05-24 23:06:09 +0545

    add about

--- resources/views/backend/common/sidebar.blade.php
+++ resources/views/backend/common/sidebar.blade.php
@@ -2,141 +2,37 @@
   <div class="menu_section">
     <h3>General</h3>
     <ul class="nav side-menu">
-      <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron"></span></a>
+      <li><a><i class="fa fa-home"></i> गृहपृष्ठ <span class="fa fa-chevron"></span></a>
       </li>
       <li><a><i class="fa fa-home"></i> हाम्रो बारेमा <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
-          <li><a href="{{route('admin.getAbout')}}">परिचय</a></li>
+          <li><a  href="{{route('admin.getAbout')}}">परिचय</a></li>
+          <li><a  href="submenu3.html">कार्यालय अन्तर्गतका निकायहरू</a></li>
+          <li><a  href="submenu3.html">कर्मचारी विवरण</a></li>
+          <li><a  href="submenu3.html">नागरिक बडापत्र</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+       <li><a><i class="fa fa-home"></i> नीति तथा कार्यक्रम <span class="fa fa-chevron"></span></a>
+      </li>
+       <li><a><i class="fa fa-home"></i> ऐन तथा नियमावली <span class="fa fa-chevron"></span></a></li>
+
+      <li><a><i class="fa fa-home"></i> हाम्रो बारेमा <span class="fa fa-chevron-down"></span></a>              
         <ul class="nav child_menu">
-          <li><a href="form.html">General Form</a></li>
-          <li><a href="form_advanced.html">Advanced Components</a></li>
-          <li><a href="form_validation.html">Form Validation</a></li>
-          <li><a href="form_wizards.html">Form Wizard</a></li>
-          <li><a href="form_upload.html">Form Upload</a></li>
-          <li><a href="form_buttons.html">Form Buttons</a></li>
+          <li><a href="form.html">सूचना</a></li>
+          <li><a href="form_advanced.html">सूचनाको हक</a></li>
+          <li><a href="form_validation.html">प्रेश विज्ञप्ति</a></li>
+          <li><a href="form_wizards.html">समाचार</a></li>
+          <li><a href="form_upload.html">बोलपत्र</a></li>
+          <li><a href="form_buttons.html">परिपत्र</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="general_elements.html">General Elements</a></li>
-          <li><a href="media_gallery.html">Media Gallery</a></li>
-          <li><a href="typography.html">Typography</a></li>
-          <li><a href="icons.html">Icons</a></li>
-          <li><a href="glyphicons.html">Glyphicons</a></li>
-          <li><a href="widgets.html">Widgets</a></li>
-          <li><a href="invoice.html">Invoice</a></li>
-          <li><a href="inbox.html">Inbox</a></li>
-          <li><a href="calendar.html">Calendar</a></li>
-        </ul>
-      </li>
-      <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="tables.html">Tables</a></li>
-          <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-        </ul>
-      </li>
-      <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="chartjs.html">Chart JS</a></li>
-          <li><a href="chartjs2.html">Chart JS2</a></li>
-          <li><a href="morisjs.html">Moris JS</a></li>
-          <li><a href="echarts.html">ECharts</a></li>
-          <li><a href="other_charts.html">Other Charts</a></li>
-        </ul>
-      </li>
-      <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-          <li><a href="fixed_footer.html">Fixed Footer</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-  <div class="menu_section">
-    <h3>Live On</h3>
-    <ul class="nav side-menu">
-      <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="e_commerce.html">E-commerce</a></li>
-          <li><a href="projects.html">Projects</a></li>
-          <li><a href="project_detail.html">Project Detail</a></li>
-          <li><a href="contacts.html">Contacts</a></li>
-          <li><a href="profile.html">Profile</a></li>
-        </ul>
-      </li>
-      <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="page_403.html">403 Error</a></li>
-          <li><a href="page_404.html">404 Error</a></li>
-          <li><a href="page_500.html">500 Error</a></li>
-          <li><a href="plain_page.html">Plain Page</a></li>
-          <li><a href="login.html">Login Page</a></li>
-          <li><a href="pricing_tables.html">Pricing Tables</a></li>
-        </ul>
-      </li>
-      <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-            <li><a href="#level1_1">Level One</a>
-            <li><a>Level One<span class="fa fa-chevron-down"></span></a>
-              <ul class="nav child_menu">
-                <li class="sub_menu"><a href="level2.html">Level Two</a>
-                </li>
-                <li><a href="#level2_1">Level Two</a>
-                </li>
-                <li><a href="#level2_2">Level Two</a>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#level1_2">Level One</a>
-            </li>
-        </ul>
-      </li>                  
-      <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+     <li><a><i class="fa fa-home"></i> क्रियाकलापहरू <span class="fa fa-chevron"></span></a></li>
+     <li><a><i class="fa fa-home"></i> हुलाक दर <span class="fa fa-chevron"></span></a></li>
+     <li><a><i class="fa fa-home"></i> सम्पर्क गर्नुहोस <span class="fa fa-chevron"></span></a></li>
+     <li><a><i class="fa fa-home"></i> नीति तथा कार्यक्रम <span class="fa fa-chevron"></span></a></li>
+    
     </ul>
   </div>
 
 </div>
 
-
-		<!-- 		<div class="nav-collapse sidebar-nav">
-					<ul class="nav nav-tabs nav-stacked main-menu">
-
-						<li><a href="/admin/"><i class="icon-home"></i><span class="hidden-tablet"> गृहपृष्ठ</span></a></li>	
-
-						<li>
-							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> हाम्रो बारेमा</span><span class="label label-important"> 5 </span></a>
-							<ul  class="dropdown-list">
-								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> परिचय</span></a></li>
-								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> हाम्रो बारेमा</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> कार्यालय अन्तर्गतका निकायहरू</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> कर्मचारी विवरण</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> नागरिक बडापत्र</span></a></li>
-							</ul>	
-						</li>
-						<li><a href="/admin/student-manage"><i class="icon-user"></i><span class="hidden-tablet"> नीति तथा कार्यक्रम</span></a></li>
-
-						<li><a href="/admin/semester-manage"><i class="icon-th"></i><span class="hidden-tablet">ऐन तथा नियमावली</span></a></li>
-						
-						<li>
-							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> हाम्रो बारेमा</span><span class="label label-important"> 5 </span></a>
-							<ul  class="dropdown-list">
-								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> सूचना</span></a></li>
-								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet">सूचनाको हक</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> प्रेश विज्ञप्ति</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> समाचार</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> बोलपत्र</span></a></li>
-								<li><a class="submenu" href="submenu3.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> परिपत्र</span></a></li>
-							</ul>	
-						</li>
-
-						<li><a href="/admin/attendance-manage"><i class="icon-book"></i><span class="hidden-tablet"> क्रियाकलापहरू</span></a></li>
-
-						<li><a href="/admin/set-location"><i class="icon-map-marker"></i><span class="hidden-tablet"> हुलाक दर</span></a></li>
-
-						<li><a href="/admin/admin-manage"><i class="icon-user"></i><span class="hidden-tablet"> सम्पर्क गर्नुहोस</span></a></li>
-						
-					</ul>
-				</div> -->
--- resources/views/backend/pages/single-pages/about.blade.php
+++ resources/views/backend/pages/single-pages/about.blade.php
@@ -1,61 +1,67 @@
 @extends('backend.layout.app')
 @section('content')
-
-<div class="x_panel">
-  <div class="x_title">
-    <h2>हाम्रो बारेमा <small>K hi sort Description Rakhunuhos hai</small></h2>
-    <ul class="nav navbar-right panel_toolbox">
-      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Settings 1</a>
-            <a class="dropdown-item" href="#">Settings 2</a>
-          </div>
-      </li>
-      <li><a class="close-link"><i class="fa fa-close"></i></a>
-      </li>
-    </ul>
-    <div class="clearfix"></div>
-  </div>
-  <div class="x_content">
-    <br />
-    <form class="form-label-left input_mask" action="{{route('admin.postAbout')}}" method="POST" >
-    	@csrf
-    	@method('POST')
-
-    	<div class="row">
-	    	<div class="col-md-6">
-			    <div class="col-md-12 col-sm-12  form-group has-feedback">
-			        <input type="text" name="title_nepali" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Nepali Title" value="{{@$about->title_nepali}}">
-			        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-			     </div>
-			     <div class="col-md-12 col-sm-12  form-group has-feedback">
-			       <textarea name="description_nepali">{{@$about->description_nepali}}</textarea>
-			     </div>
-    		</div>
-    		<div class="col-md-6">
-    			 <div class="col-md-12 col-sm-12  form-group has-feedback">
-			        <input type="text" name="title_english" class="form-control has-feedback-left" id="inputSuccess2" placeholder="English Title" value="{{@$about->title_english}}">
-			        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-			     </div>
-			     <div class="col-md-12 col-sm-12  form-group has-feedback">
-			      <textarea name="description_english">{{@$about->description_english}}</textarea>
-			     </div>
-    		</div>
-    	</div>    
-	
-
-      <div class="form-group row">
-        <div class="col-md-12 col-sm-12  offset-md-5">
-          <button type="button" class="btn btn-primary">Cancel</button>
-		          <button class="btn btn-primary" type="reset">Reset</button>
-          <button type="submit" class="btn btn-success">Submit</button>
-        </div>
+<div class="clearfix"></div>
+<div class="row">
+  <div class="col-md-12 col-sm-12 ">
+    <div class="x_panel">
+      <div class="x_title">
+        <h2>Form Design <small>different form elements</small></h2>
+        <ul class="nav navbar-right panel_toolbox">
+          <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a class="dropdown-item" href="#">Settings 1</a>
+              </li>
+              <li><a class="dropdown-item" href="#">Settings 2</a>
+              </li>
+            </ul>
+          </li>
+          <li><a class="close-link"><i class="fa fa-close"></i></a>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
       </div>
+      <div class="x_content">
+        <br />
 
-    </form>
+        <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="{{route('admin.postAbout')}}" method="POST">
+			@csrf
+          @method('POST')
+		<div class="row">
+			<div class="col-md-6">
+	           	<label class="col-form-label " for="first-name">First Name <span class="required">*</span>
+	            </label>
+	            <input type="text" name="title_nepali" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Nepali Title" value="{{@$about->title_nepali}}">
+	           	<label class="col-form-label " for="first-name">First Name <span class="required">*</span>
+	            </label>
+	            <textarea name="description_nepali">{{@$about->description_nepali}}</textarea>		           
+			</div>
+			<div class="col-md-6">
+				<label class="col-form-label " for="first-name">First Name <span class="required">*</span>
+		         </label>
+		        <input type="text" name="title_english" class="form-control has-feedback-left" id="inputSuccess2" placeholder="English Title" value="{{@$about->title_english}}">
+		           
+	           	<label class="col-form-label " for="first-name">First Name <span class="required">*</span>
+	            </label>
+	            <textarea name="description_english">{{@$about->description_english}}</textarea>
+		          
+			</div>
+		</div>
+                   
+          <div class="ln_solid"></div>
+          <div class="item form-group">
+            <div class="col-md-6 col-sm-6 offset-md-5">
+              <button class="btn btn-primary" type="button">Cancel</button>
+			  <button class="btn btn-primary" type="reset">Reset</button>
+              <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
